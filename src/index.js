4aa51336586bc6670b673c11dbc2968af43c73f4
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-09 11:57:44 -0500

    Add and style placeholder elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './style.css';
+
+const leaderboardList = document.getElementById('leaderboard-list');
+
+const placeholders = [
+  {
+    name: 'Name',
+    score: 100,
+  },
+  {
+    name: 'Name',
+    score: 20,
+  },
+  {
+    name: 'Name',
+    score: 50,
+  },
+  {
+    name: 'Name',
+    score: 78,
+  },
+];
+
+function populateList() {
+  placeholders.forEach((item, i) => {
+    const newElement = document.createElement('li');
+    newElement.innerHTML = `${item.name}: ${item.score}`;
+    if (i % 2 === 1) {
+      newElement.style.backgroundColor = '#d3d3d3';
+    }
+    leaderboardList.appendChild(newElement);
+  });
+}
+
+populateList();
