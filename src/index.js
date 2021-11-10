9b7a1afff88001549b1ad47c19bd825ab99da5b1
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-10 12:02:18 -0500

    Add event listener to refresh button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import populateList from './populateList';
 import getScores from './requests'
 
 const TEST_GAME_ID = 'IsGWyZ5ywIUXczEDTXvL'
+const refreshButton = document.getElementById('refresh-btn')
+
+refreshButton.addEventListener('click', () => {
+  getScores(TEST_GAME_ID).then(scores => {
+    populateList(scores.result);
+  })
+})
 
 getScores(TEST_GAME_ID).then(scores => {
   populateList(scores.result);
 })
+
+console.log(getScores(TEST_GAME_ID))
