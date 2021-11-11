b8aa0d7a8c301610b7524c653b8a47a53c1490ae
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-11 13:06:15 -0500

    Add style to buttons

--- src/apperance.js
+++ src/apperance.js
@@ -1,5 +1,9 @@
 const checkbox = document.getElementById('checkbox');
+const buttons = Array.from(document.getElementsByTagName('button'));
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
+  buttons.forEach((element) => {
+    element.classList.toggle('dark');
+  });
 });
