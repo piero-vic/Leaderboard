6bc8433608037a08615a1aed4688c1cfc2c6ee81
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-11 12:49:43 -0500

    Implement apperance toggle and update dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: ['./src/index.js', './src/populateList.js', './src/requests.js'],
+  entry: ['./src/index.js', './src/populateList.js', './src/requests.js', './src/apperance.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
